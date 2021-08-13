be767db9b68546f5440576193efce614d279fc94
PRANTOSH <prantoshv@gmail.com>
2021-08-13 11:46:10 +0530

    fixed dark mode bug

--- JS/type.js
+++ JS/type.js
@@ -1,13 +1,1 @@
-var typed = new Typed('.type', {
-    strings: ['Web Design', 'Machine Learning','Data Science','Full Stack'],
-    typeSpeed:50,
-    backSpeed:50,
-    loop:true
-  });
 
-  var typed = new Typed('.contact__title', {
-    strings: ['Text Us'],
-    typeSpeed:50,
-    backSpeed:50,
-    loop:true
-  });  
