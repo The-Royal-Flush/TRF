f696a879555e6b86bb4443f2dc8552d04f658cba
PRANTOSH <prantoshv@gmail.com>
2021-08-13 11:28:02 +0530

    adding typed JS in contact form

---
+++ JS/type.js
@@ -0,0 +1,13 @@
+var typed = new Typed('.type', {
+    strings: ['Web Design', 'Machine Learning','Data Science','Full Stack'],
+    typeSpeed:50,
+    backSpeed:50,
+    loop:true
+  });
+
+  var typed = new Typed('.contact__title', {
+    strings: ['Text Us'],
+    typeSpeed:50,
+    backSpeed:50,
+    loop:true
+  });  
