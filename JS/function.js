d389815552f2dec593f7a6479b6a9220f1258ebd
Hemant Kumar <39943449+hkrobotics@users.noreply.github.com>
2021-08-16 17:23:39 +0530

    merged Main Branch (#55)
    * test1
    
    * deleted files and removed some of the bugs
    
    * added dark mode themes and illustrations
    
    * added padding in nav bar
    
    * BETA-cards-team
    
    * cards test
    
    * added cards
    
    Co-authored-by: PRANTOSH <prantoshv@gmail.com>
    Co-authored-by: Prantosh Verma <68802039+leo1104@users.noreply.github.com>
    Co-authored-by: Ayan Shrivastava <Ayanshrivastava2020@gmail.com>

--- JS/function.js
+++
@@ -1,5 +0,0 @@
-var one="#0e131b";
-$(window).on("scroll touchmove", function() {
-    if ($(document).scrollTop() >= $("#portfolio").position().top) {
-    $('body').css('background', $("#one").attr("data-color"));
-    }});
